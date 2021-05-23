2183e7e4366c701fae6bc7dae0e07af692dfdb90
Islam Ayman <e-i.mohammed@lean.sa>
2021-05-23 21:35:43 +0200

    Add Level #2: en_route_salute: fix test class name

--- level_2/en_route_salute/test_en_route_salute.py
+++ level_2/en_route_salute/test_en_route_salute.py
@@ -3,7 +3,7 @@ import unittest
 from level_2.en_route_salute import answer
 
 
-class TheCakeIsNotALie(unittest.TestCase):
+class TestEnRouteSalute(unittest.TestCase):
     def test(self):
         self.assertEqual(answer('--->-><-><-->-'), 10)
         self.assertEqual(answer('>----<'), 2)
